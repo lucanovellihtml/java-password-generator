c8ab794b85646061f9623154b1fb9bd4508bd250
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-06-05 12:17:31 +0200

    add: added method to create the password

--- src/main/java/org/lessons/java/security/PasswordGenerator.java
+++ src/main/java/org/lessons/java/security/PasswordGenerator.java
@@ -5,15 +5,20 @@ public class PasswordGenerator {
 	public static void main(String[] args) {
 		
 		//DICHIARAZIONI VARIABILI
-		String name = "luca";
-		String surname = "novelli";
+		String name = "Luca";
+		String surname = "Novelli";
 		String color = "black";
-		String birth = "21-12-2000";
+		String birth = "21/12/2000";
 
 		
 		//METODO CALCOLO PASSWORD
-		String password;
-		
+		String[] date = birth.split("/");
+		int day = Integer.valueOf(date[0]);
+		int mounth = Integer.valueOf(date[1]);
+		int year = Integer.valueOf(date[2]);
+		int summ = day + mounth + year;
+		String password = name + "-" + surname + "-" + color + "-" + summ;
+		System.out.print("Password ---> " + password);
 	}
 
 }
