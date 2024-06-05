07259f3d4d69685b084500173500062836086231
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-06-05 11:35:07 +0200

    add: variable declaration

---
+++ src/main/java/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,19 @@
+package org.lessons.java.security;
+
+public class PasswordGenerator {
+
+	public static void main(String[] args) {
+		
+		//DICHIARAZIONI VARIABILI
+		String name = "luca";
+		String surname = "novelli";
+		String color = "black";
+		String birth = "21-12-2000";
+
+		
+		//METODO CALCOLO PASSWORD
+		String password;
+		
+	}
+
+}
